7cc98700a6507049330e9b78c4439253dc059023
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-02 22:08:45 +0900

    権限が許可されたらカメラをスタートさせる準備を実装

--- app/src/main/java/net/kwmt27/cameraxapp/MainActivity.kt
+++ app/src/main/java/net/kwmt27/cameraxapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package net.kwmt27.cameraxapp
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.TextureView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 
 private const val REQUEST_CODE_PERMISSIONS = 10
 
@@ -10,8 +14,39 @@ private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewFinder: TextureView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun startCamera() {
+        // TODO
+    }
+
+    private fun updateTransform() {
+        // TODO
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                viewFinder.post { startCamera() }
+            } else {
+                Toast.makeText(this, "Permissions not grated by the user.", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted(): Boolean {
+        for (permission in REQUIRED_PERMISSIONS) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false
+            }
+        }
+        return true
+    }
+
 }
