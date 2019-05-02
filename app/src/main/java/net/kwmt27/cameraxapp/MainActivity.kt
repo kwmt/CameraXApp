ed8066c3d8df6e5717ed7e8eeec241d57645e959
Yasutaka Kawamoto <kwmt27@gmail.com>
2019-05-02 22:01:53 +0900

    ランタイムパーミッションで使うための準備

--- app/src/main/java/net/kwmt27/cameraxapp/MainActivity.kt
+++ app/src/main/java/net/kwmt27/cameraxapp/MainActivity.kt
@@ -1,7 +1,12 @@
 package net.kwmt27.cameraxapp
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class MainActivity : AppCompatActivity() {
 
